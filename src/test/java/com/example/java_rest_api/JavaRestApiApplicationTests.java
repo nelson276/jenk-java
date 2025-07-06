93fda5ec15314f40c6ddb184fd0ca80416a169a2
nelson276 <nelsondsouza590@gmail.com>
2025-07-06 12:53:47 +0530

    jekins java initial

---
+++ src/test/java/com/example/java_rest_api/JavaRestApiApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.java_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test()
+	void test1(){
+		System.out.println("Test running");
+	}
+
+}
